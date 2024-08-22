2687120f985bdd96f3a14cb5ce43c1b0afcdfba0
Flor Escobar <florescobar919@gmail.com>
2024-08-22 09:30:13 +0200

    building a first custom pipe

---
+++ src/app/temperature.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "temperature",
+  standalone: true
+})
+
+export class TemperaturePipe implements PipeTransform {
+
+  transform(value: any, ...args: any[]) {
+    return value + '_transformed';
+  }  
+}
